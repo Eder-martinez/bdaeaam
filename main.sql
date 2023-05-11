24ab5b5de6d6c9838032d3905c370ea8a0323e24
Eder-martinez <122801090+Eder-martinez@users.noreply.github.com>
2023-05-11 15:34:26 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,11 @@
+SELECT "Hello world!";
+create table personas(
+	id_persona	integer primary key	autoincrement,
+	nombre		text 		not null,
+	primer_apellido	text 		not null,
+	segundo_apellido	text 		not null,
+	email 		text 		not null);
+insert into personas (nombre, primer_apellido, segundo_apellido, email) values
+		('Eder Abimael', 'Arias', 'Martinez', 'ederarias@ejemplo.com'),
+		('Sara', 'Escorcia', 'Ruiz', 'saraescorcia@ejemplo.com');
+select * from personas; 	
